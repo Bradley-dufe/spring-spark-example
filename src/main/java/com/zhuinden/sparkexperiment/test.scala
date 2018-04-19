a110312bfac7fa169c759588ca12111a9d470942
yanglei118 <yanglei30@jd.com>
2018-04-19 21:40:42 +0800

    test

---
+++ src/main/java/com/zhuinden/sparkexperiment/test.scala
@@ -0,0 +1,13 @@
+package com.zhuinden.sparkexperiment
+
+/**
+  * Author: YangLei(ERP:yanglei118)
+  * Description:
+  * Date: Create in 下午9:40 2018/4/19
+  * update in 下午9:40 2018/4/19
+  */
+object test {
+  def sayHello: Unit ={
+    println("hello")
+  }
+}
