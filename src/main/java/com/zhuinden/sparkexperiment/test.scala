23ff5dfa95de640dbb4bf9521694f50e58b24d75
yanglei118 <yanglei30@jd.com>
2018-04-20 14:25:22 +0800

    test

--- src/main/java/com/zhuinden/sparkexperiment/test.scala
+++ src/main/java/com/zhuinden/sparkexperiment/test.scala
@@ -9,15 +9,12 @@ import org.springframework.stereotype.Component
   * Date: Create in 下午9:40 2018/4/19
   * update in 下午9:40 2018/4/19
   */
-@Component
 class test {
-
   def main(args: Array[String]): Unit ={
     for(i <- 1 to 100){
       Thread.sleep(2000)
     }
   }
-  @Scheduled(fixedDelay = 4000)
   def sayHello: Unit ={
     println("hello")
   }
